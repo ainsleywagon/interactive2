0d96c270de3e101dbd9f9ab49e5c9facce31efca
ainsleywagon <ainsley.wagoner@gmail.com>
2018-01-17 23:51:58 -0800

    Trying some 50 results examples

--- exercises/exercise-charrette/assets/script.js
+++ exercises/exercise-charrette/assets/script.js
@@ -1,11 +1,24 @@
 $(document).ready(function(){
-	
+
 	$('#button-3').click(function() {
 		$(this).animate({
 			opacity: 0.4,
 			marginLeft: '0.6in',
 			fontSize: '24px',
 			borderWidth: '10px'
+		}, 1000 ); //duration
+	});
+
+	$('#button-5').click(function() {
+		$(this).animate({
+			opacity: 0.4,
+			fontSize: '24px',
+		}, 500 );
+	});
+
+	$("#button-6").click(function(){
+		$(this).animate({
+				backgroundColor: "#fafafa"
 		}, 1000 );
 	});
 
